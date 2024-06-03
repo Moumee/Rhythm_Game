a739b096f64ed1b49eea07c135c1df2ff33333f4
Moumee <zzuzzu1009@naver.com>
2024-06-04 04:36:12 +0900

    Changed video to newer version

--- Assets/Script/IntroManager.cs
+++ Assets/Script/IntroManager.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class IntroManager : MonoBehaviour
 {
-    public void GoSlideScene()
+    VideoPlayer introVideoPlayer;
+    private void Awake()
     {
-        SceneManager.LoadScene("SlideScene");
+        introVideoPlayer = GetComponent<VideoPlayer>();
+    }
+    public void PlayIntroVideo()
+    {
+        introVideoPlayer.Play();
+        
     }
 }
