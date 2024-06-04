436133563d51e1f96a3e7be43af7b6f3d3e60d98
Moumee <zzuzzu1009@naver.com>
2024-06-05 01:37:16 +0900

    Added first stage scene

---
+++ Assets/Script/IngredientMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientMove : MonoBehaviour
+{
+    [SerializeField] Vector2 startPosition;
+    [SerializeField] Vector2 endPosition;
+    [SerializeField] Vector2 centerPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
