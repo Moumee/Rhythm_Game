d38008a52462480a6b2e4835cf1a4136ec22e511
Moumee <zzuzzu1009@naver.com>
2024-06-16 00:55:13 +0900

    Punch Animation

--- Assets/Script/Punch.cs
+++ Assets/Script/Punch.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Punch : MonoBehaviour
 {
+    float punchDuration = 0.1f;
+    float upDuration = 0.2f;
+    float elapsedTime = 0f;
+    Vector3 startPos;
+    Animator animator;
     // Start is called before the first frame update
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        startPos = transform.position;
+    }
     public void OnEvent_Punch()
     {
         StartCoroutine(PunchSuccess());
@@ -13,8 +23,22 @@ public class Punch : MonoBehaviour
 
     IEnumerator PunchSuccess()
     {
-        transform.position += Vector3.down * 8.5f;
+        elapsedTime = 0f;
+        while (elapsedTime < punchDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, startPos + new Vector3(0, -8.5f, 0), elapsedTime / punchDuration);
+            yield return null;
+        }
+        animator.SetTrigger("Punch");
         yield return new WaitForSeconds(0.2f);
-        transform.position -= Vector3.down * 8.5f;
+        elapsedTime = 0f;
+        while (elapsedTime < upDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos + new Vector3(0, -8.5f, 0), startPos, elapsedTime / upDuration);
+            yield return null;
+        }
+
     }
 }
