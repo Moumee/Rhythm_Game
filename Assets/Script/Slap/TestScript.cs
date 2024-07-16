d03b6371ef9a5d658866367737252cdcedd97aa3
Moumee <zzuzzu1009@naver.com>
2024-07-16 14:31:21 +0900

    Added TestScript for Fish Slap

--- Assets/Script/Slap/TestScript.cs
+++ Assets/Script/Slap/TestScript.cs
@@ -7,6 +7,7 @@ public class TestScript : MonoBehaviour
     [SerializeField] SlapPool slapObjectPool;
     [SerializeField] Transform leftSlapPosition;
     [SerializeField] Transform rightSlapPosition;
+    [SerializeField] Animator fishAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +22,14 @@ public class TestScript : MonoBehaviour
             GameObject leftSlap = slapObjectPool.GetLeftSlap();
             leftSlap.transform.position = leftSlapPosition.position;
             leftSlap.SetActive(true);
+            fishAnim.SetTrigger("LeftHit");
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             GameObject rightSlap = slapObjectPool.GetRightSlap();
             rightSlap.transform.position = rightSlapPosition.position;
             rightSlap.SetActive(true);
+            fishAnim.SetTrigger("RightHit");
         }
     }
 }
