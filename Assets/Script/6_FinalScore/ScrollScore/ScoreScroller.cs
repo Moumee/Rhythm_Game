dbc3fdc9887f74b90f229e1d7ab027a265e2d6e6
Seunghwan <slee1009@khu.ac.kr>
2024-10-10 16:01:58 +0900

    [FEAT] 점수 스크롤 구현

---
+++ Assets/Script/6_FinalScore/ScrollScore/ScoreScroller.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+
+public class ScoreScroller : MonoBehaviour
+{
+    public RectTransform unitsRT;
+    public RectTransform tensRT;
+    public RectTransform hundredsRT;
+    public RectTransform thousandsRT;
+    
+    
+    public GameObject numberPrefab;
+    private int height = 100;
+    private int score = 0604; 
+    public int cycles = 10;
+    
+    private void Awake()
+    {
+        InitDigit(score / 1000, thousandsRT);
+        InitDigit((score % 1000) / 100, hundredsRT);
+        InitDigit((score % 100) / 10, tensRT);
+        InitDigit(score % 10, unitsRT);
+        StartCoroutine(MoveUnitsCoroutine());
+        StartCoroutine(MoveDigitCoroutine(tensRT, 2));
+        StartCoroutine(MoveDigitCoroutine(hundredsRT, 4)); 
+        StartCoroutine(MoveDigitCoroutine(thousandsRT, 6));    
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    
+    void InitDigit(int targetNumber, RectTransform RT)
+    {
+        int steps = cycles * 10;
+        for (int i = 0; i < steps; i++)
+        {
+            GameObject numberObj = Instantiate(numberPrefab, RT.transform);
+            RectTransform numberRT = numberObj.GetComponent<RectTransform>();
+            numberRT.anchoredPosition = new Vector2(0, (steps - i) * height);
+            TextMeshProUGUI numberTMP = numberObj.GetComponent<TextMeshProUGUI>();
+            numberTMP.text = ((i + targetNumber) % 10).ToString();
+        }
+        
+    }
+
+    IEnumerator MoveUnitsCoroutine()
+    {
+        Tween tween = unitsRT.DOAnchorPosY(-cycles * height * 10, 3.4f, true)
+            .SetEase(Ease.OutCubic);
+        yield return tween.WaitForCompletion();
+    }
+
+    IEnumerator MoveDigitCoroutine( RectTransform inRT, int cycleSubtract)
+    {
+        Tween tween = inRT.DOAnchorPosY(-cycles * height * 10, 3.4f, true)
+            .SetEase(Ease.OutCubic);
+        while (tween.IsActive() && !tween.IsComplete())
+        {
+            if (inRT.anchoredPosition.y <= -cycles * height * 10 + 100 * cycleSubtract * 10)
+            {
+                tween.Kill();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
