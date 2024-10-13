71bf72bc0cfc0d2bc209c1816e5743aa1da6f479
Park-YW <snake010807@khu.ac.kr>
2024-10-13 21:44:06 +0900

    replie logic create

--- Assets/Script/6_FinalScore/Reply.cs
+++ Assets/Script/6_FinalScore/Reply.cs
@@ -1,26 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Reply : MonoBehaviour
 {
-    private Vector3 moveDistance = new Vector3 (-48f, 182f, 0f);
+    private Vector3 moveDistance = new Vector3(-48f, 182f, 0f);
+    private int position;
     private Image sprite;
+    private Vector3 destination;
+    private RectTransform rect;
+
+    private bool isApear = false, isSlide = false;
+
     // Start is called before the first frame update
     void Awake()
     {
         sprite = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
         sprite.CrossFadeAlpha(0f, 0f, true);
     }
 
-    public void Apear()
+    public void initialize(Sprite spr, int postionNum)
     {
-
+        sprite.sprite = spr;
+        rect.position += (- postionNum) * moveDistance;
+        destination = rect.position;
     }
+
+    public void Apear()
+    {  
+        sprite.CrossFadeAlpha(1f, 0.5f, false);
+        destination = rect.position;
+        rect.position -= moveDistance.normalized * 50;
+    }
+
 
     public void Slide()
     {
+        destination = rect.position + moveDistance;
+    }
 
+    public void Update()
+    {
+        if ((rect.position - destination).magnitude > 1f)
+        {
+            rect.position += moveDistance.normalized * 700 * Time.deltaTime;
+        }
+        else
+        {
+            rect.position = destination;
+        }
+        
     }
 }
